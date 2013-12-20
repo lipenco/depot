d0af3b415aa92fb7e947b4aadbc1a925f980972d
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-12-20 12:39:03 +0800

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'd136b00a7738ce17fb2925c6ac2ba4d74b54779a05161639eca13a3315118a8c2cea9803715d973bdcb6b918dcbae784c150e90954b7eeb4ddc2cb92b47fdad7'
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.delete_all
+Product.create!(title: 'Programming Ruby', description: %{ruby is cool, is dinamic and so one.</p>}, image_url: 'ruby.jpg', price: 13.99)
+Product.create!(title: 'Programming LALA', description: %{ruby is cool, is dinamic and so one.</p>}, image_url: 'ruby.jpg', price: 14.99)
+Product.create!(title: 'Programming sdada', description: %{ruby is cool, is dinamic and so one.</p>}, image_url: 'ruby.jpg', price: 15.99)
+Product.create!(title: 'Programming sadas', description: %{ruby is cool, is dinamic and so one.</p>}, image_url: 'ruby.jpg', price: 16.99)
+Product.create!(title: 'Programming dsadad', description: %{ruby is cool, is dinamic and so one.</p>}, image_url: 'ruby.jpg', price: 17.99)
