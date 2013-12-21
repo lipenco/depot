a2c0c2f3e57916e40a99d76dbf8a75b69188dac0
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-12-21 20:43:25 +0800

    adding cart partial

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,7 @@
 class CartsController < ApplicationController
-  include CurrentCart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
-
+  
   # GET /carts
   # GET /carts.json
   def index
@@ -11,7 +10,7 @@ class CartsController < ApplicationController
 
   # GET /carts/1
   # GET /carts/1.json
-  def show
+  def show 
   end
 
   # GET /carts/new
@@ -26,7 +25,7 @@ class CartsController < ApplicationController
   # POST /carts
   # POST /carts.json
   def create
-    @cart = Cart.new(params[:id])
+    @cart = Cart.new(cart_params)
 
     respond_to do |format|
       if @cart.save
@@ -56,28 +55,28 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy if cart.is == session[:cart_id]
-    session[:cart_id] = nil
+    @cart.destroy if @cart.id == session[:cart_id]
+        session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'your cart is currently empty' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
     end
 
-    def invalid_cart
-      logger.error "attempt to access invalid cart #{params[:id]}"
-      redirect_to store_url, notice: 'invalid cart'
-    end
-
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
-    end
-end
+   end        
+     
+         def invalid_cart
+                logger.error "Attempt to access invalid cart #{params[:id]}"
+                        redirect_to store_url, notice: 'Invalid cart' 
+                end
+   
+        end
