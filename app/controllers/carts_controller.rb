b06cf3694fd5900a59d7e1e993625b1b684b8329
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-12-21 19:12:18 +0800

    invalid card - setting the error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
+  include CurrentCart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -24,7 +26,7 @@ class CartsController < ApplicationController
   # POST /carts
   # POST /carts.json
   def create
-    @cart = Cart.new(cart_params)
+    @cart = Cart.new(params[:id])
 
     respond_to do |format|
       if @cart.save
@@ -66,6 +68,12 @@ class CartsController < ApplicationController
     def set_cart
       @cart = Cart.find(params[:id])
     end
+
+    def invalid_cart
+      logger.error "attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'invalid cart'
+    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
